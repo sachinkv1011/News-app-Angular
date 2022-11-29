c73bc4f19eafdd30cbf1635f61554375c2e62ed9
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-29 13:37:01 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsLiveComponent } from './news-live/news-live.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const route:Routes=[
+  {
+    path:"",
+    component:NewsLiveComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NewsLiveComponent
+    NewsLiveComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
